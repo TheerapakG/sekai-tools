c865c7e70f781a77b2f91255b10ed961a7be2054
TheerapakG <theerapakg@gmail.com>
2023-03-16 13:32:04 +0700

    initial commit

---
+++ list_musics.py
@@ -0,0 +1,94 @@
+from pjsekai.models.master_data import MusicCategory, MusicDifficulty, MusicDifficultyType, ReleaseCondition, ResourceBox, ResourceBoxPurpose, ResourceBoxType, ResourceType
+from pjsekai.enums.enums import MusicDifficultyType
+from pjsekai.enums.unknown import Unknown
+from pjsekai.client import Client
+import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CATEGORY = {MusicCategory.MV_2D.value: "2d", MusicCategory.MV.value: "3d", MusicCategory.ORIGINAL.value: "original"}
+
+print("starting client ...")
+client = Client(
+    bytes(os.environ["KEY"], encoding="utf-8"),
+    bytes(os.environ["IV"], encoding="utf-8"),
+    system_info_file_path="./system-info.json",
+    master_data_file_path="./master-data.json",
+    user_data_file_path="./user-data.json",
+    asset_directory="./asset",
+)
+print("updating data ...")
+client.update_all()
+
+difficulties_dict: dict[int, dict[MusicDifficultyType | Unknown, MusicDifficulty]] = {}
+if difficulties := client.master_data.music_difficulties:
+    for difficulty in difficulties:
+        if (music_id := difficulty.music_id) and (music_difficulty := difficulty.music_difficulty):
+            difficulty_types_dict = difficulties_dict.get(music_id, {})
+            difficulty_types_dict[music_difficulty] = difficulty
+            difficulties_dict[music_id] = difficulty_types_dict
+
+release_conditions_dict: dict[int, ReleaseCondition] = {}
+if release_conditions := client.master_data.release_conditions:
+    for condition in release_conditions:
+        if (condition_id := condition.id):
+            release_conditions_dict[condition_id] = condition
+
+music_resource_boxes_dict: dict[int, ResourceBox] = {}
+if resource_boxes := client.master_data.resource_boxes:
+    for resource_box in resource_boxes:
+        if resource_box.resource_box_purpose == ResourceBoxPurpose.SHOP_ITEM and resource_box.resource_box_type == ResourceBoxType.EXPAND:
+            if details := resource_box.details:
+                for detail in details:
+                    if (detail.resource_type == ResourceType.MUSIC) and (resource_id := detail.resource_id):
+                        music_resource_boxes_dict[resource_id] = resource_box
+
+if musics := client.master_data.musics:
+    unreleased_musics = filter(lambda music: music.published_at and music.published_at > datetime.datetime.now(datetime.timezone.utc), musics)
+    # unreleased_musics = musics
+    for unreleased_music in sorted(unreleased_musics, key=lambda music: music.published_at if music.published_at else -1):
+        music_published_at = unreleased_music.published_at.date() if unreleased_music.published_at else None
+        music_id = unreleased_music.id
+        music_categories = [category for category in unreleased_music.categories if category != MusicCategory.IMAGE] if unreleased_music.categories else []
+        music_categories_str = f"[{'|'.join([CATEGORY[category.value] for category in music_categories if category.value and CATEGORY.get(category.value)])}]"if music_categories else ""    
+        music_resource_box = music_resource_boxes_dict.get(music_id) if music_id else None
+        music_resource_box_id = music_resource_box.id if music_resource_box else None
+        music_ids_str = " ".join([s for s in [f'm: {music_id}' if music_id else '', f'r: {music_resource_box_id}' if music_resource_box_id else ''] if s])
+        music_difficulties = difficulties_dict.get(music_id, {}) if music_id else {}
+        music_difficulty_inner_str = "/".join(map( \
+            lambda music_difficulty: str(music_difficulty.play_level) if music_difficulty and music_difficulty.play_level else '??', \
+            map(lambda difficulty: music_difficulties.get(difficulty), MusicDifficultyType) \
+        ))
+        music_difficulty_str = f"LV: {music_difficulty_inner_str}"
+        music_condition_id = unreleased_music.release_condition_id
+        music_condition_str = None
+        match music_condition_id:
+            case None:
+                music_condition_str = "??"
+            case 1:
+                music_condition_str = "unlocked"
+            case 5:
+                music_condition_str = "shop: 2"
+            case 10:
+                music_condition_str = "present"
+            case _:
+                music_condition = release_conditions_dict.get(music_condition_id)
+                music_condition_str = f"{music_condition_id}: {music_condition.sentence}" if music_condition else "??"
+        out_str = " | ".join([
+            " ".join([
+                s 
+                for s 
+                in [
+                    f"**{unreleased_music.title}**",
+                    music_categories_str,
+                    str(music_published_at)
+                ]
+                if s
+            ]),
+            music_difficulty_str,
+            music_ids_str,
+            music_condition_str
+        ])
+        print(out_str)
---
+++ load_assets.py
@@ -0,0 +1,147 @@
+from pjsekai.client import Client
+import UnityPy
+from UnityPy.tools.extractor import EXPORT_TYPES, export_obj
+
+import shutil
+import subprocess
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print("starting client ...")
+client = Client(
+    bytes(os.environ["KEY"], encoding="utf-8"),
+    bytes(os.environ["IV"], encoding="utf-8"),
+    system_info_file_path="./system-info.json",
+    master_data_file_path="./master-data.json",
+    user_data_file_path="./user-data.json",
+    asset_directory="./asset",
+)
+print("updating data ...")
+client.update_all()
+print("logging in ...")
+client.login(int(os.environ["USER_ID"]), os.environ["CREDENTIAL"])
+
+export_types_keys = list(EXPORT_TYPES.keys())
+
+def defaulted_export_index(type):
+    try:
+        return export_types_keys.index(type)
+    except (IndexError, ValueError):
+        return 999
+    
+directory = client.asset_directory
+if not directory:
+    raise ValueError("no asset directory")
+
+asset = client.asset
+if not asset:
+    raise ValueError("no asset")
+
+if not (asset.asset_bundle_info and asset.asset_bundle_info.version == asset.version):
+    print("updating asset bundle info ...")
+    asset_bundle_info = asset.get_asset_bundle_info(client.api_manager)
+else:
+    print("asset bundle info already updated ...")
+    asset_bundle_info = asset.asset_bundle_info
+
+bundles = asset_bundle_info.bundles
+if not bundles:
+    raise ValueError("no bundles")
+
+
+def load_asset(asset_bundle_str: str, force: bool = False) -> bool:
+    bundle_hash = bundles[asset_bundle_str].hash
+    if not bundle_hash:
+        print(f"no bundle for {asset_bundle_str}")
+        return False
+    
+    if (directory / "bundle" / f"{asset_bundle_str}.unity3d").exists():
+        try:
+            with open(directory / "hash" / asset_bundle_str, "r") as f:
+                if f.read() == bundle_hash and not force:
+                    print(f"bundle {asset_bundle_str} already updated")
+                    return False
+        except FileNotFoundError:
+            pass
+        
+        print(f"updating bundle {asset_bundle_str}")
+    else:
+        print(f"downloading bundle {asset_bundle_str}")
+
+    with client.api_manager.download_asset_bundle(asset_bundle_str) as asset_bundle:
+        (directory / "bundle" / asset_bundle_str).parent.mkdir(parents=True, exist_ok=True)
+        with open(directory / "bundle" / f"{asset_bundle_str}.unity3d", "wb") as f:
+            for chunk in asset_bundle.chunks:
+                f.write(chunk)
+
+        env = UnityPy.load(str(directory / "bundle" / f"{asset_bundle_str}.unity3d"))
+        container = sorted(
+            env.container.items(), key=lambda x: defaulted_export_index(x[1].type)
+        )
+
+        for obj_path, obj in container:
+            obj_path = "/".join(x for x in obj_path.split("/") if x)
+            obj_dest = directory / obj_path
+            obj_dest.parent.mkdir(parents=True, exist_ok=True)
+
+            print(f"extracting {obj_path}")
+
+            export_obj(
+                obj, # type: ignore
+                obj_dest,
+            )
+
+            if obj_dest.suffixes == [".acb", ".bytes"]:
+                shutil.copy(obj_dest, obj_dest.with_suffix(""))
+                subprocess.run(["./vgmstream-cli", "-o", obj_dest.parent / "?n.wav", "-S", "0", obj_dest.with_suffix("")])
+    
+    (directory / "hash" / asset_bundle_str).parent.mkdir(parents=True, exist_ok=True)
+    with open(directory / "hash" / asset_bundle_str, "x") as f:
+        f.write(bundle_hash)
+            
+    print(f"updated bundle {asset_bundle_str}")
+    return True
+
+if __name__ == "__main__":
+    master_data = client.master_data
+    if not master_data:
+        raise ValueError("no master data")
+    
+    _musics = master_data.musics
+    if not _musics:
+        raise ValueError("no musics")
+    musics = {music.id: music for music in _musics if music.id}
+    
+    music_vocals = master_data.music_vocals
+    if not music_vocals:
+        raise ValueError("no music vocals")
+    
+    asset_bundles =  []
+
+    asset_bundles.extend([
+        # "live/2dmode/background/default",
+        # "live/note/default",
+        "live/tap_se/default",
+        "music/long/se_0333_01"
+    ])
+    
+    asset_bundles.extend((bundle.bundle_name 
+        for bundle in bundles.values() 
+        if bundle.bundle_name and \
+            bundle.bundle_name.startswith("music/music_score")
+    ))
+
+    for vocal in music_vocals:
+        if vocal.seq == 1:
+            if asset_bundle_name := vocal.asset_bundle_name:
+                asset_bundles.append(f"music/long/{asset_bundle_name}")
+            else:
+                print(f"cannot find vocal bundle for {musics.get(vocal.music_id, f'music id {vocal.music_id}') if vocal.music_id else 'unknown music'}")
+
+    updated_bundles = [*filter(load_asset, asset_bundles)]
+
+    print(f"updated {len(updated_bundles)} bundles")
+    for bundle in updated_bundles:
+        print(f" - {bundle}")
